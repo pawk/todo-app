21207fbf049d6e0dc2a49ce32c59e3c4567ea290
Paweł Kornijów <pawel.kornijow@intive.com>
2018-06-11 15:18:00 +0200

    get rid of ref in Add component

--- src/todo/add.jsx
+++ src/todo/add.jsx
@@ -4,25 +4,23 @@ import PropTypes from 'prop-types';
 import './add.css';
 
 export const TodoAdd = ({ onAdd }) => {
-  const inputRef = React.createRef();
-
-  const handleAdd = () => {
-    const content = inputRef.current.value;
+  const handleAdd = e => {
+    const content = e.target.value;
     if (content) {
       onAdd({ content });
-      inputRef.current.value = '';
+      e.target.value = '';
     }
   }
 
   const handleKeyUp = e => {
     if (e.keyCode === 13 || e.which === 13) {
-      handleAdd();
+      handleAdd(e);
     }
   }
 
   return (
     <section className="todo__add">
-      <input className="todo__add-input" ref={inputRef} onKeyUp={handleKeyUp} placeholder="Next thing to do is ..." />
+      <input className="todo__add-input"onKeyUp={handleKeyUp} placeholder="Next thing to do is ..." />
       <button className="todo__add-btn" onClick={handleAdd}>+</button>
     </section>
   );
