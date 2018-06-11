bda91f83c6b2b04bbf29500fb19760d0b95cf015
Paweł Kornijów <pawel.kornijow@intive.com>
2018-06-11 23:08:32 +0200

    filter buttons

--- src/todo/filter.jsx
+++ src/todo/filter.jsx
@@ -9,9 +9,16 @@ const handleKeyUp = fn => e => {
   fn(e.target.value);
 };
 
+const filterAll = e => { console.log('filter all'); }
+const filterDone = e => { console.log('filter done'); }
+const filterPending = e => { console.log('filter pending'); }
+
 export default function TodoFilter({ onFilter }) {
   return (
     <section>
+      <button onClick={filterAll}>All</button>
+      <button onClick={filterDone}>Done</button>
+      <button onClick={filterPending}>Pending</button>
       <input type="text"
         className="todo__filter-input"
         onKeyUp={handleKeyUp(onFilter)}
