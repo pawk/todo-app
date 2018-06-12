34df3d2cd9d5463eb88d461132a76f9ffd1bb064
Paweł Kornijów <pawel.kornijow@intive.com>
2018-06-12 11:35:22 +0200

    nope, I don't like this add button

--- src/todo/add.jsx
+++ src/todo/add.jsx
@@ -4,26 +4,23 @@ import PropTypes from 'prop-types';
 import './add.css';
 
 export const TodoAdd = ({ onAdd }) => {
-  const ref = React.createRef();
-
-  const handleAdd = () => {
-    const content = ref.current.value;
+  const handleAdd = e => {
+    const content = e.target.value;
     if (content) {
       onAdd({ content });
-      ref.current.value = '';
+      e.target.value = '';
     }
   }
 
   const handleKeyUp = e => {
     if (e.keyCode === 13 || e.which === 13) {
-      handleAdd();
+      handleAdd(e);
     }
   }
 
   return (
     <section className="todo__add">
-      <input className="todo__add-input" ref={ref} onKeyUp={handleKeyUp} placeholder="Enter new item" />
-      <button class="todo__add-btn" onClick={handleAdd}>+</button>
+      <input className="todo__add-input"onKeyUp={handleKeyUp} placeholder="Enter new item" />
     </section>
   );
 };
